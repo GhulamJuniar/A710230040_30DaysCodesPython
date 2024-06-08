908a55589b6099742c7987d8e0ed1541d336a138
Ghuulaam <a710230040@student.ums.ac.id>
2024-06-08 18:13:39 +0700

    Create BelajarTipeData.py
    - Day 2 - Belajar Tipe Data -

---
+++ Day 2/BelajarTipeData.py
@@ -0,0 +1,30 @@
+# Integer
+umur = 18
+print ("Umur:", umur)
+
+# Float
+tinggi = 173
+print ("Tinggi:", tinggi)
+
+# String
+nama = "Ghulam"
+print("Nama:", nama)
+
+# Boolean
+is_student = True
+print ("Is Student:", is_student)
+
+# List
+buah = ["Apel", "Nangka", "Durian"]
+
+# Tuple
+koordinat = (10, 20)
+print("Koordinat:", koordinat)
+
+# Dictionary
+data_mahasiswa = {"nama": "Ghulam", "Umur": 18, "is student" : True}
+print("Data Mahasiswa:", data_mahasiswa)
+
+# Set
+angka_unik = {1, 2, 3, 4, 5}
+print("Angka Unik:", angka_unik)
