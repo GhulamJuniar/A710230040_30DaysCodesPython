46ed76cb57a2c2d399ef7b5bfae114cc9df705fc
Ghuulaam <a710230040@student.ums.ac.id>
2024-06-10 17:58:17 +0700

    Create BelajarLooping.py
    Day - 4 Belajar Looping

---
+++ Day 4/BelajarLooping.py
@@ -0,0 +1,11 @@
+# Loop While Hitung Angka Dari 1-5
+i = 1
+while i <=5:
+    print(f"While Loop Looping Ke-{i}")
+
+
+    for j in range(i,i+1):
+        print(f" For Loop Looping Ke-{j}")
+
+       
+        i +=1
