5c06f6fa3f7c0e566cfd12cd167e98ac672e2a3c
Ghuulaam <a710230040@student.ums.ac.id>
2024-06-16 21:16:33 +0700

    Create BelajadStringManipulation.py
    - Day 10 - Belajar String Manipulation

---
+++ Z Day 10/BelajadStringManipulation.py
@@ -0,0 +1,6 @@
+pesan = " Halo, Nama Saya Ghulam" 
+print(pesan.upper()) # Untuk Mengubah Tulisan Menjadi Kapital
+
+print(pesan.lower()) # Untuk Mengubah Tulisan Menjadi Kecil
+
+print(pesan.split()) # Untuk Memisahkan Kalimat Tulisan
